46702ed4e557e03e09a3c196defbcad1dedbced2
furusapo <kodomojyanaimon@gmail.com>
2026-07-23 19:27:40 +0900

    業者問い合わせ用GASへ接続

--- docs/config.js
+++ docs/config.js
@@ -1,1 +1,1 @@
-window.GAS_URL = 'https://script.google.com/macros/s/AKfycbw76FbUpKO0bdZ-ImjH9-O5CcMA7Ns7KEU0vtZ_lborFXKmtH1bieqnnFRNofII8eTg/exec';
+window.GAS_URL = 'https://script.google.com/macros/s/AKfycbxcy9u2bu96dX3B8yNw1c3wnu3uw4yt6if9QCLXRk0DyVB_Dki1Qe_h63vt9qIx7e37SQ/exec';
